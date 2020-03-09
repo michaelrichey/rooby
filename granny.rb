021b6e60fdf257bda0bde9d3eebb019f97551419
michaelrichey <michael@tofugu.com>
2020-03-09 12:10:49 -0400

    add granny program

---
+++ granny.rb
@@ -0,0 +1,10 @@
+puts 'Hello, sonny.'
+
+ask = gets.chomp
+
+while ask != ask.upcase
+    puts 'What\'s that you say, sonny? Speak up there, sonny!'
+    ask = gets.chomp
+end
+
+puts 'Yes, I can here you just fine, sonny.'
